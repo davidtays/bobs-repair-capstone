b15900bdc4c5be8f3762f26a80920ae0a7682019
effects67 <32003168+effects67@users.noreply.github.com>
2018-10-25 15:24:18 -0500

    more auth

--- bobs/src/app/authentication.service.ts
+++ bobs/src/app/authentication.service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { map } from 'rxjs/operators/map';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+
 
 //interfaces
 
@@ -23,12 +26,63 @@ export class AuthenticationService{
     }
     return this.token;
   }
+  // payload
+  public getUserDetails(): UserDetails{
+    const token = this.getToken();
+    let payload;
+    if (token) {
+      payload = token.split('.')[1];
+      payload = window.atob(payload); // atob decodes Base64
+      return JSON.parse(payload);
+    } else {
+      return null;
+    }
+  }
+
+  public isLoggedIn(): boolean{
+    const user = this.getUserDetails();
+    if (user) {
+      return user.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  }
+  // API call
+  private request(method: 'post'|'get',type:'login'|'register'|'profile', user?: TokenPayload): Observable<any> {
+    let base;
+
+    if (method === 'post'){
+      base = this.http.post('/api/${type}', user);
+    } else {
+      base = this.http.get('api${type}', {headers:{Authorization:'Bearer ${this.getToken()'}});
+    }
+    
+    const request =base.pipe(
+      map((data:TokenResponse)=>{
+        if (data.token){
+          this.saveToken(data.token);
+        }
+        return data;
+      })
+    );
+    return request;
+  }
+  public register(user: TokenPayload):Observable<any>{
+    return this.request('post','register',user);
+  }
+  public login(user:TokenPayload):Observable<any>{
+    return this.request('post','login',user);
+  }
+  
   public logout(): void{
     this.token = '';
     window.localStorage.removeItem('mean-token');
     this.router.navigateByUrl('/');
   }
 }
+
+
+
 
 //uses HTTP client inside angular to make http requests to server application
 export interface UserDetails{
