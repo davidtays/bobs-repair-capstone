d2ac21370e301898da7328d0dc4fc436601aeee0
davidtays <davidtays22@gmail.com>
2018-10-25 16:41:36 -0500

    pushing minor changes, updated user.js schema

--- bobs/src/app/authentication.service.ts
+++ bobs/src/app/authentication.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
 
 
 
